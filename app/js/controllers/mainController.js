6ee983f40b73719a9f9f0e79182d459c5960c15e
DeRosario <davidveiga.pereira@gmail.com>
2017-03-23 17:42:05 +0100

    david

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -5,10 +5,20 @@ angular.module('app')
         $scope.query = "";
         $scope.goSearch = function() {
 
-            // OMDB API
+            // webcam API
             webcamService.getOne($scope.query).then(function(response) {
                 $scope.details = response.data;
-                console.log($scope.details);
+                console.log($scope.details.result.webcams[0].id);
+
+
+
+
+
+//2EME ETAPE
+                var idcam = $scope.details.result.webcams[0].id;
+                console.log(idcam);
+
+
 
             });
         };

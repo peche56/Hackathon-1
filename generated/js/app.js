9e3497445e9e2755ed58cdafdb8fdaf581675761
DeRosario <davidveiga.pereira@gmail.com>
2017-03-24 00:12:06 +0100

    carte markers placés

--- generated/js/app.js
+++ generated/js/app.js
@@ -58119,7 +58119,72 @@ angular.module("app").run(["$templateCache", function($templateCache) {
   );
 
   $templateCache.put("anon/map.html",
-    "<h1>Hello World</h1>"
+    "<div id=\"map\" style=\"width: 100%; height: 500px;\"></div>\n" +
+    "\n" +
+    "  <script type=\"text/javascript\">\n" +
+    "    var locations = [\n" +
+    "      //EUROPE\n" +
+    "      ['Paris ', 48.85661400000001, 2.3522219000000177],\n" +
+    "      ['Madrid', 40.4167754, -3.7037901999999576],\n" +
+    "      ['Lisbonne', 38.7222524, -9.139336599999979],\n" +
+    "      ['Rome', 41.9027835, 12.496365500000024],\n" +
+    "      ['Londres', 51.5073509, -0.12775829999998223],\n" +
+    "      ['Berlin', 52.52000659999999, 13.404953999999975],\n" +
+    "      ['Varsovie', 52.2296756, 21.012228700000037],\n" +
+    "      ['Moscou', 55.755826, 37.6173],\n" +
+    "      //ASIE\n" +
+    "      ['Tokyo', 35.6894875, 139.69170639999993],\n" +
+    "      ['Pékin', 39.904211, 116.40739499999995],\n" +
+    "      ['Bangkok', 13.7563309, 100.50176510000006],\n" +
+    "      ['Singapour', 1.3553794, 103.86774439999999],\n" +
+    "      ['Ankara', 39.9333635, 32.85974190000002],\n" +
+    "      ['Jérusalem', 31.768319, 35.21370999999999],\n" +
+    "      ['Séoul', 37.566535, 126.97796919999996],\n" +
+    "      //AMERIQUE\n" +
+    "      ['New York', 40.660639, -73.981934],\n" +
+    "      ['Washington', 38.819167, -77.01416],\n" +
+    "      ['LosAngeles', 33.976165, -118.234863],\n" +
+    "      ['Mexico,', -33.665836, -99.118652],\n" +
+    "      ['Santiago', -33.950198, -70.751953],\n" +
+    "      ['Brasilia', -15.978225, -47.8125],\n" +
+    "      ['Buenos Aires', -34.972397, -58.183594],\n" +
+    "      //OCEANIE\n" +
+    "      ['Canberra', -35.3075, 149.124417],\n" +
+    "      ['Wellington', -41.2864603, 174.77623600000004],\n" +
+    "      //AFRIQUE\n" +
+    "      ['Pretoria', -9.067379373933349, 17.6220703125],\n" +
+    "      ['Alger', 36.753768, 3.0587560999999823,],\n" +
+    "      ['Nairobi', -1.322635, 36.804199],\n" +
+    "      ['Brazzaville,', -9.067379373933349, 17.6220703125],\n" +
+    "      ['Le Caire', 30.023448, 31.201172],\n" +
+    "      ['Rabat', 33.972522 , -6.899414],\n" +
+    "      ['Tunis', 36.731842, 10.151367],\n" +
+    "    ];\n" +
+    "\n" +
+    "    var map = new google.maps.Map(document.getElementById('map'), {\n" +
+    "      zoom: 3,\n" +
+    "      center: new google.maps.LatLng(40, 0),\n" +
+    "      mapTypeId: google.maps.MapTypeId.ROADMAP\n" +
+    "    });\n" +
+    "\n" +
+    "    var infowindow = new google.maps.InfoWindow();\n" +
+    "\n" +
+    "    var marker, i;\n" +
+    "\n" +
+    "    for (i = 0; i < locations.length; i++) {\n" +
+    "      marker = new google.maps.Marker({\n" +
+    "        position: new google.maps.LatLng(locations[i][1], locations[i][2]),\n" +
+    "        map: map\n" +
+    "      });\n" +
+    "\n" +
+    "      google.maps.event.addListener(marker, 'click', (function(marker, i) {\n" +
+    "        return function() {\n" +
+    "          infowindow.setContent(locations[i][0]);\n" +
+    "          infowindow.open(map, marker);\n" +
+    "        }\n" +
+    "      })(marker, i));\n" +
+    "    }\n" +
+    "  </script>\n"
   );
 
   $templateCache.put("anon/navbar.html",
